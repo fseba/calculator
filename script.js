438884db8f6d3f46bbda6901a6f635c7dcfed70e
sebastianfrueh <sfrueh@gmx.net>
2022-06-26 17:14:31 +0200

    Add comments

--- script.js
+++ script.js
@@ -40,27 +40,27 @@ function operate(a, operation, b) {
     } 
 }
 
-// populate input
 
 let displayValue = '';
 let operator; 
 let lastOperator; 
 let firstValue; 
 let secondValue;
-let firstValueSet = false;
-let secondValueSet = false; 
+let firstValueSet = false; //to check if the value may be overwritten
+let secondValueSet = false; //to check if the value may be overwritten
 
 const inputField = document.querySelector('.input-field'); 
 const inputButtons = document.querySelectorAll('.input-buttons');
 const operatorButtons = document.querySelectorAll('.grid-operator-buttons button');
 const equalButton = document.getElementById('equal'); 
 
+
 function populate(input) {
   displayValue += input;
   inputField.textContent = displayValue;
 }
 
-function setValue(input) {
+function setValue(input) { //Set values, depending on which value is set 
   if (!firstValueSet) {
     firstValue = input; 
     firstValueSet = true; 
@@ -80,39 +80,40 @@ inputButtons.forEach(button => {
 operatorButtons.forEach(button => {
   button.addEventListener('click', () => {
     operatorButtons.forEach(button => {
-      if(button.classList.contains('operator-buttons-clicked')) { // to remove the 'operator-buttons-clicked' class if other operator button is clicked
+      if(button.classList.contains('operator-buttons-clicked')) { //to remove the 'operator-buttons-clicked' class if other operator button is clicked
         button.classList.toggle('operator-buttons-clicked');
       };
     });
 
-    if(button.textContent === '=') return; // to prevent the operator being set to '=', but remove 'operator-buttons-clicked' class when equal button is pressed
+    if(button.textContent === '=') return; //to prevent the operator being set to '=', but remove 'operator-buttons-clicked' class when equal button is pressed
     
     if(firstValueSet && operator === '') {
-      lastOperator = operator = button.textContent;
+      operator = lastOperator = button.textContent;
       button.classList.toggle('operator-buttons-clicked');
       displayValue = ''; 
       return; 
     }
 
-    if(firstValueSet && operator !== '') {
-      setValue(displayValue);
+    if(firstValueSet && operator !== '') { //to handle the case if an operator key is pressed to concatenate calculations
+      setValue(displayValue);  
       firstValue = operate(firstValue, operator, secondValue); 
       populate(firstValue);
       secondValueSet = false; 
       displayValue = '';
-    } else {
+    } 
+    
+    else {
       setValue(displayValue);
     };
-    lastOperator = operator = button.textContent;
+    
+    operator = lastOperator = button.textContent;
     button.classList.toggle('operator-buttons-clicked'); 
-
-
     })
   });
 
 
 equalButton.addEventListener('click', () => {
-  if(firstValue === undefined) return; //error handling for when the equal key is pressed without values being set
+  if(firstValue === undefined) return; //error handling for if the equal key is pressed without values being set
 
   if(operator === '') { //makes it possible to press the equal key several times to repeat the calculation
     operator = lastOperator; 
@@ -124,7 +125,7 @@ equalButton.addEventListener('click', () => {
   firstValue = operate(firstValue, operator, secondValue); 
   secondValueSet = false; 
 
-  if(isNaN(firstValue)) { //error handling for when the equal key is pressed without values being set
+  if(isNaN(firstValue)) { //error handling for if the equal key is pressed without values being set
     populate('Error');
     firstValueSet = false; 
   } else {
